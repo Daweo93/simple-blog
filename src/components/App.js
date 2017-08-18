770efad8e5a971a8b5fc3fa7dcb5da40ca8f3d06
Dawid H <dawiddaweo@gmail.com>
2017-08-18 16:34:46 +0200

    Install react router, add main route to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PostsIndex from './postIndex';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <div className="row">
-          Let get it started.
-        </div>
+      <div className="row">
+        <BrowserRouter>
+          <div>
+            <Route path="/" component={PostsIndex} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
